bef2bc1a9da6fb59b6e15238c4ad5aa742ffe226
vishal5553 <101632391+vishal5553@users.noreply.github.com>
2025-05-27 13:53:53 +0530

    Add files via upload

---
+++ WebMvc_Thymeleaf/src/main/java/com/app/WebMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ WebMvc_Thymeleaf/src/main/java/com/app/controller/welcomeController.java
@@ -0,0 +1,17 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class welcomeController {
+	@GetMapping("/")
+	public String getWelcomeMsg(Model model) {
+		model.addAttribute("mesg", "welcome to Tdit");
+		
+		
+		return "index";
+	}
+
+}
